bef881a84ca96b1e3030fdee6f8db80e652692c8
Ashwini Aphale <61335510+ashwiniaphale@users.noreply.github.com>
2022-03-11 21:02:32 -0500

    eslint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-shadow */
+/* eslint-disable padded-blocks */
+/* eslint-disable arrow-parens */
+/* eslint-disable camelcase */
+/* eslint-disable quote-props */
+/* eslint-disable arrow-body-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 import { useState, useEffect } from 'react';
 
@@ -9,7 +20,7 @@ function App() {
     fetch('/reviewfromdb').then(response => response.json()).then(data => {
       setComment(data);
     });
-  }, [])
+  }, []);
 
   function deleteReview(id) {
     const newRatings = list_all_comments.filter(list_all_comments => list_all_comments.id !== id);
@@ -17,16 +28,14 @@ function App() {
   }
 
   function saveAllChanges(request) {
-    fetch("/reviewfromid", {
-      method: "POST",
+    fetch('/reviewfromid', {
+      method: 'POST',
       headers: {
-        "content_type": "application/json",
+        'content_type': 'application/json',
       },
       body: JSON.stringify(request)
-    }
-    ).then(response => { return response.json() })
-      .then(console.log(request))
-    alert("Your changes were saved.")
+    }).then((response) => { return response.json(); });
+    alert('Your changes were saved.');
   }
 
   return (
@@ -34,18 +43,27 @@ function App() {
       <center>
         <h2>Your reviews:</h2>
         <table>
-          {list_all_comments && list_all_comments.map((list_all_comments) =>
+          {list_all_comments && list_all_comments.map((list_all_comments) => (
             <tr key={list_all_comments.id}>
-              <tb><b>Movie ID: {list_all_comments.movie_id}</b></tb>&nbsp;
-              <tb><input type="text" defaultValue={list_all_comments.rating}></input></tb> &nbsp;
-              <tb><input type="text" defaultValue={list_all_comments.reviews}></input></tb>&nbsp;
+              <tb>
+                <b>
+                  Movie ID:
+                  {list_all_comments.movie_id}
+                </b>
+              </tb>
+              &nbsp;
+              <tb><input type="text" defaultValue={list_all_comments.rating} /></tb>
+              {' '}
+              &nbsp;
+              <tb><input type="text" defaultValue={list_all_comments.reviews} /></tb>
+              &nbsp;
               <button onClick={() => deleteReview(list_all_comments.id)}>Delete</button>
             </tr>
-          )}
+          ))}
         </table>
         <button onClick={() => saveAllChanges(list_all_comments)}>Save All Changes</button>
       </center>
-    </div >
+    </div>
   );
 }
 export default App;
